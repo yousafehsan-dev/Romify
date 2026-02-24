bac8b68eea04af4913d6403fca3e0cc078b290e6
yousafehsan-dev <yousafehsan@125.com>
2026-02-24 17:28:57 +0500

    implement homepage ui

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,19 +1,95 @@
-import type { Route } from "./+types/home";
+import type {Route} from "./+types/home";
 import Navbar from "../../components/Navbar"
+import {ArrowRight, ArrowUpRight, Clock, Layers} from "lucide-react";
+import Button from "../../components/ui/Button";
 
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    return [
+        {title: "New React Router App"},
+        {name: "description", content: "Welcome to React Router!"},
+    ];
 }
 
 export default function Home() {
-  return(
-  <div className="home">
+    return (
+        <div className="home">
+            <Navbar/>
+            <section className="hero">
+                <div className="announce">
+                    <div className="dot">
+                        <div className="pulse"></div>
+                    </div>
+                    <p> Introducing Romify 2.0</p>
+                </div>
+                <h1> Build beautiful spaces at the speed of thought with Roomify</h1>
 
-    <Navbar />
-    <h1 className="text-3xl text-indigo-700 font-extrabold"> yousaf </h1>;
-  </div>)
+                <p className="subtitle">
+                    ROOMIFY IS AN AI-FIRST DESIGN ENVIRONMENT THAT
+                    HELPS YOU VISUALIZE, RENDER, AND SHIP ARCHITECTURAL
+                    PROJECTS FASTER THAN EVER.
+                </p>
+                <div className="actions">
+                    <a href="#upload" className="cta">
+                        Start Building <ArrowRight className="icon"/>
+                    </a>
+                    <Button variant="outline" size="lg" className="demo">
+                        Watch Demo
+                    </Button>
+                </div>
+
+                <div id="upload" className="upload-shell">
+                    <div className="grid-overlay"/>
+                    <div className="upload-card">
+                        <div className="upload-head">
+                            <div className="upload-icon">
+                                <Layers className="icon"/>
+                            </div>
+                            <h3> Upload your floor plan</h3>
+                            <p> Support JPY,PNG, formats UP to 10MB</p>
+                        </div>
+                        <p> Upload Image </p>
+                    </div>
+                </div>
+
+
+            </section>
+            <section className="projects">
+                <div className="section-inner">
+                    <div className="section-head">
+                        <div className="copy">
+                            <h2> Projects</h2>
+                            <p> Your Latest Projects and Community Project all in on place</p>
+                        </div>
+                    </div>
+                    <div className="projects-grid">
+                        <div className="project-card group">
+                            <div className="preview">
+                                <img
+                                    src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
+
+                                    alt="project"/>
+                                <div className="badge">
+                                    <span>Community</span>
+                                </div>
+                            </div>
+                            <div className="card-body">
+                                <div>
+                                    <h3>Project manthana</h3>
+                                    <div className="meta">
+                                        <Clock size={12}/>
+                                        <span>{new Date('01.01.2027').toLocaleDateString()}</span>
+                                        <span> By Yousaf Chughtai </span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="arrow">
+                                <ArrowUpRight size={12}/>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+        </div>)
 }
