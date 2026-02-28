b51227847fedab640b99aa4907065cf025796c42
yousafehsan-dev <yousafehsan@125.com>
2026-03-01 01:18:02 +0500

    3dfs

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,111 +1,158 @@
-import type {Route} from "./+types/home";
-import Navbar from "../../components/Navbar"
+import type { Route } from "./+types/home";
+import Navbar from "../../components/Navbar";
 import {ArrowRight, ArrowUpRight, Clock, Layers} from "lucide-react";
 import Button from "../../components/ui/Button";
 import Upload from "../../components/Upload";
 import {useNavigate} from "react-router";
-
-
+import {useEffect, useRef, useState} from "react";
+import {createProject, getProjects} from "../../lib/puter.action";
 
 export function meta({}: Route.MetaArgs) {
     return [
-        {title: "New React Router App"},
-        {name: "description", content: "Welcome to React Router!"},
+        { title: "New React Router App" },
+        { name: "description", content: "Welcome to React Router!" },
     ];
 }
 
 export default function Home() {
-
     const navigate = useNavigate();
+    const [projects, setProjects] = useState<DesignItem[]>([]);
+    const isCreatingProjectRef = useRef(false);
 
-    const handleUploadComplete = async (base64Image: string) =>{
+    const handleUploadComplete = async (base64Image: string) => {
+        try {
 
-        const newId = Date.now().toString();
-        navigate(`/visualizer/${newId}`);
-        return true;
+            if(isCreatingProjectRef.current) return false;
+            isCreatingProjectRef.current = true;
+            const newId = Date.now().toString();
+            const name = `Residence ${newId}`;
+
+            const newItem = {
+                id: newId, name, sourceImage: base64Image,
+                renderedImage: undefined,
+                timestamp: Date.now()
+            }
+
+            const saved = await createProject({ item: newItem, visibility: 'private' });
+
+            if(!saved) {
+                console.error("Failed to create project");
+                return false;
+            }
+
+            setProjects((prev) => [saved, ...prev]);
+
+            navigate(`/visualizer/${newId}`, {
+                state: {
+                    initialImage: saved.sourceImage,
+                    initialRendered: saved.renderedImage || null,
+                    name
+                }
+            });
+
+            return true;
+        } finally {
+            isCreatingProjectRef.current = false;
+        }
     }
 
+    useEffect(() => {
+        const fetchProjects = async () => {
+            const items = await getProjects();
+
+            setProjects(items)
+        }
+
+        fetchProjects();
+    }, []);
 
     return (
         <div className="home">
-            <Navbar/>
+            <Navbar />
+
             <section className="hero">
                 <div className="announce">
                     <div className="dot">
                         <div className="pulse"></div>
                     </div>
-                    <p> Introducing Romify 2.0</p>
+
+                    <p>Introducing Roomify 2.0</p>
                 </div>
-                <h1> Build beautiful spaces at the speed of thought with Roomify</h1>
+
+                <h1>Build beautiful spaces at the speed of thought with Roomify</h1>
 
                 <p className="subtitle">
-                    ROOMIFY IS AN AI-FIRST DESIGN ENVIRONMENT THAT
-                    HELPS YOU VISUALIZE, RENDER, AND SHIP ARCHITECTURAL
-                    PROJECTS FASTER THAN EVER.
+                    Roomify is an AI-first design environment that helps you visualize, render, and ship architectural projects faster  than ever.
                 </p>
+
                 <div className="actions">
                     <a href="#upload" className="cta">
-                        Start Building <ArrowRight className="icon"/>
+                        Start Building <ArrowRight className="icon" />
                     </a>
+
                     <Button variant="outline" size="lg" className="demo">
                         Watch Demo
                     </Button>
                 </div>
 
                 <div id="upload" className="upload-shell">
-                    <div className="grid-overlay"/>
+                    <div className="grid-overlay" />
+
                     <div className="upload-card">
                         <div className="upload-head">
                             <div className="upload-icon">
-                                <Layers className="icon"/>
+                                <Layers className="icon" />
                             </div>
-                            <h3> Upload your floor plan</h3>
-                            <p> Support JPY,PNG, formats UP to 10MB</p>
-                        </div>
-                        <Upload onComplete={handleUploadComplete}/>
-                            // Handle redirection or next steps here
 
+                            <h3>Upload your floor plan</h3>
+                            <p>Supports JPG, PNG, formats up to 10MB</p>
+                        </div>
+
+                        <Upload onComplete={handleUploadComplete} />
                     </div>
                 </div>
-
-
             </section>
+
             <section className="projects">
                 <div className="section-inner">
                     <div className="section-head">
                         <div className="copy">
-                            <h2> Projects</h2>
-                            <p> Your Latest Projects and Community Project all in on place</p>
+                            <h2>Projects</h2>
+                            <p>Your latest work and shared community projects, all in one place.</p>
                         </div>
                     </div>
-                    <div className="projects-grid">
-                        <div className="project-card group">
-                            <div className="preview">
-                                <img
-                                    src="https://roomify-mlhuk267-dfwu1i.puter.site/projects/1770803585402/rendered.png"
 
-                                    alt="project"/>
-                                <div className="badge">
-                                    <span>Community</span>
+                    <div className="projects-grid">
+                        {projects.map(({id, name, renderedImage, sourceImage, timestamp}) => (
+                            <div key={id} className="project-card group" onClick={() => navigate(`/visualizer/${id}`)}>
+                                <div className="preview">
+                                    <img  src={renderedImage || sourceImage} alt="Project"
+                                    />
+
+                                    <div className="badge">
+                                        <span>Community</span>
+                                    </div>
                                 </div>
-                            </div>
-                            <div className="card-body">
-                                <div>
-                                    <h3>Project manthana</h3>
-                                    <div className="meta">
-                                        <Clock size={12}/>
-                                        <span>{new Date('01.01.2027').toLocaleDateString()}</span>
-                                        <span> By Yousaf Chughtai </span>
+
+                                <div className="card-body">
+                                    <div>
+                                        <h3>{name}</h3>
+
+                                        <div className="meta">
+                                            <Clock size={12} />
+                                            <span>{new Date(timestamp).toLocaleDateString()}</span>
+                                            <span>By JS Mastery</span>
+                                        </div>
+                                    </div>
+                                    <div className="arrow">
+                                        <ArrowUpRight size={18} />
                                     </div>
                                 </div>
                             </div>
-                            <div className="arrow">
-                                <ArrowUpRight size={12}/>
-                            </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
             </section>
-
-        </div>)
+        </div>
+    )
 }
